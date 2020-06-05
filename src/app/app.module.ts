041a9f35f61bacfcfa6aa246b98bfbc7200e0dd1
Rákóczi, Botond <brakoczi@itron.com>
2020-06-05 16:12:03 +0200

    sections toolbar animations scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +19,12 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCheckboxModule,
+    MatToolbarModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    ScrollToModule.forRoot(),
+    AnimateOnScrollModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
